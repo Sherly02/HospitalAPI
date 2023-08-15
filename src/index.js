8e354cffe731dbaa6ec19fbee119f8eda5a11ce9
Sherly Tanaya Mauren <sherly.tanaya@majoo.id>
2023-08-15 18:53:16 +0700

    set response by httpstatus

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import Response from './domain/response.js';
+import HttpStatus from './controller/patient.controller.js';
 import logger from './util/logger.js';
 
 dotenv.config();
@@ -12,6 +13,6 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.get('/', (request, response) => response.send(
-    new Response(200, 'OK', 'Patient API V1')
+    new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API V1')
 ));
 app.listen(PORT, ()=> logger.info(`Server running on: ${ip.address()}:${PORT}`));
