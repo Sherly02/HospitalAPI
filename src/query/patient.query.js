ea3c0c8452014b7c3fd9f86772e99cc56029491b
Sherly Tanaya Mauren <sherly.tanaya@majoo.id>
2023-08-15 21:28:50 +0700

    fix non prural word

--- src/query/patient.query.js
+++ src/query/patient.query.js
@@ -1,9 +1,9 @@
 const QUERY = {
     SELECT_PATIENTS: 'SELECT * FROM patients ORDER BY created_at DESC LIMIT 100',
     SELECT_PATIENT: 'SELECT * FROM patients WHERE id = ?',
-    CREATE_PATIENTS: 'INSERT INTO patients(first_name, last_name, email, address, diagnosis, phone, image_url) VALUES (?, ?, ?, ?, ?, ?, ?)',
-    UPDATE_PATIENTS: 'UPDATE patients SET first_name = ?, last_name = ?, email = ?, address = ?, diagnosis = ?, phone = ?, image_url = ?',
-    DELETE_PATIENTS: 'DELETE FROM patients WHERE id = ?'
+    CREATE_PATIENT: 'INSERT INTO patients(first_name, last_name, email, address, diagnosis, phone, image_url) VALUES (?, ?, ?, ?, ?, ?, ?)',
+    UPDATE_PATIENT: 'UPDATE patients SET first_name = ?, last_name = ?, email = ?, address = ?, diagnosis = ?, phone = ?, image_url = ?',
+    DELETE_PATIENT: 'DELETE FROM patients WHERE id = ?'
 };
 
 export default QUERY;
