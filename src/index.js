c3d460ff9c163bc841a50d9d628b5100af5032e6
Sherly Tanaya Mauren <sherly.tanaya@majoo.id>
2023-08-15 21:43:09 +0700

    setup routes for patient APIs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import Response from './domain/response.js';
 import HttpStatus from './controller/patient.controller.js';
+import patientRoutes from './route/patient.route.js';
 import logger from './util/logger.js';
 
 dotenv.config();
@@ -12,7 +13,11 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
+app.use('/patients', patientRoutes);
 app.get('/', (request, response) => response.send(
-    new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API V1')
+    new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Hospital API V1')
+));
+app.all('*', (request, response) => response.status(HttpStatus.NOT_FOUND.code).send(
+    new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route not found')
 ));
 app.listen(PORT, ()=> logger.info(`Server running on: ${ip.address()}:${PORT}`));
